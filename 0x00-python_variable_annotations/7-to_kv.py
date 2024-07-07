69268f2e22ab7d08d8d41e3d6908bc29fe209f17
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-07 09:17:24 +0300

    task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Convert to key and value"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple:
+    """ Convert to key and value"""
+    return (k, v ** 2)
