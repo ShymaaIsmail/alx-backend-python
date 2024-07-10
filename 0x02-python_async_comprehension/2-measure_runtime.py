ddbca5d42d10701800730600143ecc5852ec30e6
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-10 09:19:46 +0300

    task 2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
-
+"""Measure Time"""
 import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """measure_runtime"""
+    start_time = time.perf_counter()
+    routine_list = []
+    for _ in range(0, 4):
+        routine_list.append(async_comprehension())
+    await asyncio.gather(*routine_list)
+    elapsed = time.perf_counter() - start_time
+    return elapsed
