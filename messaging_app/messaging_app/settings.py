1436fd551c0e35390c55905d821f5c27b1e363e4
shymaaismail <shymaa.m.ismail@gmail.com>
2025-06-21 02:47:12 +0300

    `Update DEFAULT_PAGINATION_CLASS to MessagePagination in settings.py and remove CustomPagination class`

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -113,7 +113,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'chats.pagination.CustomPagination',
+    'DEFAULT_PAGINATION_CLASS': 'chats.pagination.MessagePagination',
     'PAGE_SIZE': 20,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
