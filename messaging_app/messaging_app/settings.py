79ee0ba750838be89dd52a2e5ac014756696664e
shymaaismail <shymaa.m.ismail@gmail.com>
2025-06-21 02:39:30 +0300

    `Removed BasicAuthentication from JWTAuthentication in REST_FRAMEWORK settings`

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -106,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.BasicAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
